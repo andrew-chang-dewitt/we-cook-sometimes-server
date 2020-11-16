77b479aa5e38f2df4dd5478075e6edd59370a2db
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-11-16 15:25:34 -0500

    setup app skeleton from seed
    - alter w/ port optionally read from process env
    - change root to `/api` to match nginx setup

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
+
+const app = express()
+const port = process.env.PORT || 8001
+
+app.use(morgan('dev'))
+
+app.get('/api', (req, res) => {
+  res.send('Hello world! tsc-watch some more changes')
+})
+
+app.listen(port, () => {
+  console.debug(`server started at localhost:${port}`)
+})
