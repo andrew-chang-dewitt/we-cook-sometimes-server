d9edefc387ae1321fe786ad8702e967bdcd6c306
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-11-18 14:39:24 -0500

    Add MongoDB connection skeleton
    - Remove html, API serves json only
    - Add environment management for storing database secrets
    - Add connection string in server script

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import morgan from 'morgan'
 import path from 'path'
+import { MongoClient } from 'mongodb'
 
 const app = express()
 const port = process.env.PORT || 8001
+const dbpassword = process.env.DBPASS
+const dbname = 'test'
+
+const mongo = new MongoClient(
+  `mongodb+srv://devtest:${dbpassword}@cluster0.2pcqv.mongodb.net/${dbname}?retryWrites=true&w=majority`
+)
 
 app.use(morgan('dev'))
 
